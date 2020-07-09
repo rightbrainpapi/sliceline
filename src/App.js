41fe9bedb8d58c5119a6c2f598f3996b5b576bfb
rightbrainpapi <dahuemaen@gmail.com>
2020-07-09 07:17:29 -0400

    installed styled-components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 body{
-
-  background-color: blue;
+  font-family: 'Open Sans', sans-serif;
 }
+
+h1, h2 , h3:
+font-family: 'Righteous', cursive;
 `;
 
 function App() {
 	return (
 		<React.Fragment>
+			<h1>Sliceline</h1>
 			<GlobalStyle />
 			<div>Hello SliceLine</div>
 		</React.Fragment>
